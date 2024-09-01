f1fe8e931e965932db718d189dd9c3cc8b4f451b
Mauricio Belforte <belymau@hotmail.com>
2024-08-31 22:30:31 -0300

    Clase 7 Estados completa

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,8 +1,40 @@
+import React from 'react';
 import './CreateTodoButton.css';
+
+
+
+
+function TodoClick(){
+    let [state, setState] = React.useState(0);
+    return(
+        <>
+            <p>
+                Diste click {state} veces    
+            </p>
+            <button onClick={()=> {
+                setState(state++)
+            }
+
+            }>
+                Click
+            </button>
+        </>
+    )
+}
 
 function CreateTodoButton() {
     return (
-        <button className="CreateTodoButton">+</button>
+        <button 
+        className="CreateTodoButton" 
+        onClick={
+            (event) => {
+                console.log("Le diste click");
+                console.log(event);
+                console.log(event.target);
+                }
+
+            }
+        >+</button>
     );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,35 @@
+import React from 'react'
 import './TodoSearch.css';
 
 function TodoSearch() {
+  // Declaramos una variable de estado llamada "searchValue" y una función para actualizarla "setSearchValue"
+  // useState('') inicializa el estado "searchValue" con el valor string vacio '' ya que los usuarios en un principio no ven nada
+  // const [searchValue, setSearchValue] desestructura el array devuelto por useState en dos variables:
+  // searchValue: contiene el valor actual del estado (inicialmente '').
+  // setSearchValue: es una función que se usa para actualizar el valor de searchValue.
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log("Aca esta el searchValue " + searchValue);
+  /* Esto se le podria llamar Renderizado del Input */
   return (
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+        }
+
+      }
     />
   );
 }
+
+
+//- Explicacion de Copilot:
+//- El valor del input está vinculado a searchValue.  value={searchValue} 
+//- Usas el evento onChange para actualizar searchValue cada vez que el usuario escribe en el input. 
+//- onChange={(event) => {setSearchValue(event.target.value);}
+
 
 export { TodoSearch };
